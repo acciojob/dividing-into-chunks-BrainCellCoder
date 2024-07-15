7e69442def0c76862e1005c5d81d40051b6c62af
BrainCellCoder <70462882+BrainCellCoder@users.noreply.github.com>
2024-07-15 21:50:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
-const arr = [1, 2, 3, 4, 1, 0, 2, 2];
+function divide(arr, n) {
+  const result = [];
+  let currentSubarray = [];
+  let currentSum = 0;
 
-const divide = (arr, n) => {
-  // Write your code here
-};
+  for (const num of arr) {
+    if (currentSum + num <= n) {
+      currentSubarray.push(num);
+      currentSum += num;
+    } else {
+      result.push(currentSubarray);
+      currentSubarray = [num];
+      currentSum = num;
+    }
+  }
 
-const n = prompt("Enter n: ");
-alert(JSON.stringify(divide(arr, n)));
+  if (currentSubarray.length > 0) {
+    result.push(currentSubarray);
+  }
+
+  return result;
+}
+
+// Prompt user for input
+// const arrInput = prompt("Enter array elements separated by commas:");
+// const n = Number(prompt("Enter the maximum sum for subarrays:"));
+// const arr = arrInput.split(',').map(Number);
+
+// alert(JSON.stringify(divide(arr, n)))
+
+
+
